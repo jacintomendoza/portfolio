3ccd73ddb60188f76d009f13321e411a9e32e384
Josh Mendoza <jjm121197@gmail.com>
2022-02-25 17:48:04 -0600

    before deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'portfolio-frontend';
+  opened = false;
+  toolbar_variable=false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+   
+  // https://stackoverflow.com/questions/59148204/angular-8-hostlistenerwindowscroll-not-working
+  // Mat-toolbar adjustments
+  @HostListener("window:scroll", [])
+  onWindowScroll(){
+    console.log(window.scrollY);
+    if (window.scrollY > 900){
+      this.toolbar_variable=true;
+    }
+    else {
+      this.toolbar_variable=false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
