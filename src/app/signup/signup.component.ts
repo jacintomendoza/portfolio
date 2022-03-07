66404a6e4153de00072b5fc17ce37f8bbae12d51
Josh Mendoza <jjm121197@gmail.com>
2022-03-07 16:37:09 -0600

    added validators

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -19,14 +19,24 @@ export class SignupComponent implements OnInit {
     this.firstFormGroup = this._formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required]
-
-
-    });
+      email: ['', [Validators.required,Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    },{validator: this.checkPasswords});
     // this.secondFormGroup = this._formBuilder.group({
     //   secondCtrl: ['', Validators.required],
     // });
+  }
+
+  // https://stackoverflow.com/questions/64109148/password-match-validation-in-reactive-form-in-angular
+  checkPasswords(group: FormGroup) {
+        const pass = group.controls['password'].value;
+        const confirmPass = group.controls['confirmPassword'].value;
+
+        return pass === confirmPass ? null : { notSame: true };
+    }
+
+  submit() {
   }
 
 }
