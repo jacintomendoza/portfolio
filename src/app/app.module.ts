4993a7b6ed0471b5c5bdb1befaa6683fbf89b2ce
Josh Mendoza <jjm121197@gmail.com>
2022-03-14 14:10:20 -0500

    added parallax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { SigninComponent } from './signin/signin.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { EducationComponent } from './education/education.component';
+import { ParallaxDirective } from './parallax.directive';
 
 
 
@@ -25,7 +26,8 @@ import { EducationComponent } from './education/education.component';
     SignupComponent,
     SigninComponent,
     ProjectsComponent,
-    EducationComponent
+    EducationComponent,
+    ParallaxDirective
   ],
   imports: [
     BrowserModule,
